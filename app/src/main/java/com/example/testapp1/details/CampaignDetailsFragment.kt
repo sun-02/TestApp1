d7cfcca1b7f3abf77577d7c0689a0db2f844e5de
sun02 <sun0280-sub1@yahoo.com>
2022-09-11 22:46:06 +0300

    Added CampaignDetailsFragment

--- app/src/main/java/com/example/testapp1/details/CampaignDetailsFragment.kt
+++ app/src/main/java/com/example/testapp1/details/CampaignDetailsFragment.kt
@@ -1,13 +1,26 @@
 package com.example.testapp1.details
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.annotation.ColorInt
+import androidx.lifecycle.lifecycleScope
 import com.example.testapp1.databinding.FragmentCampaignDetailsBinding
+import com.example.testapp1.login.step.setImageFromUrl
 import com.example.testapp1.search.Campaign
 import com.example.testapp1.search.CampaignsAdapter
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 private const val CAMPAIGN = "Campaign"
 private const val TAG = "CampaignDetailsFragment"
@@ -41,15 +54,56 @@ class CampaignDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val str = """
-            ${campaign.id}
-            ${campaign.name}
-            ${campaign.cashback}
-            ${campaign.actions}
-            ${campaign.imageUrl}
-            ${campaign.paymentTime}
-        """.trimIndent()
+        with(binding) {
+            lifecycleScope.launch(Dispatchers.IO) {
+                fcdIvCampaign.setImageFromUrl(campaign.imageUrl)
+            }
+            fcdTvName.text = campaign.name
+            fcdTvCashback.text = campaign.cashback
+            fcdTvPaymentTime.text = campaign.paymentTime
 
-//        binding.tv.text = str
+            val typedValue = TypedValue()
+            val theme = requireContext().theme
+            theme.resolveAttribute(android.R.attr.colorAccent, typedValue, true)
+            @ColorInt val colorAccent = typedValue.data
+
+            val actionsSpanned = campaign.actions.map{ action ->
+                SpannableString("${action.value} ${action.text}").apply {
+                    setSpan(
+                        ForegroundColorSpan(colorAccent),
+                        0,
+                        action.value.length,
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
+                }
+            }
+
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            val marginMedium = requireActivity()
+                .resources.getDimensionPixelSize(com.example.testapp1.R.dimen.margin_medium)
+            val marginSmall = requireActivity()
+                .resources.getDimensionPixelSize(com.example.testapp1.R.dimen.margin_small)
+            layoutParams.setMargins(marginMedium, marginSmall, marginMedium, 0)
+
+            for (act in actionsSpanned) {
+                val textView = TextView(this@CampaignDetailsFragment.requireContext())
+                textView.layoutParams = layoutParams
+                textView.setTextAppearance(com.example.testapp1.R.style.TextAppearance_TestApp1_Body1_Bold)
+                textView.text = act
+                fcdLl.addView(textView)
+            }
+
+            fcdBtnBuy.setOnClickListener { v ->
+                Snackbar.make(v, "Bought!", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
